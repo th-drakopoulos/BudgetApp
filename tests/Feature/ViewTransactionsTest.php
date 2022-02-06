8b494ce43e266042c8876aadee70b744b6aa10a3
th-drakopoulos <th.drakopoulos@tms.com.gr>
2022-02-07 00:01:45 +0200

    lecture3

---
+++ tests/Feature/ViewTransactionsTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Transaction;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ViewTransactionsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function it_can_display_all_transactions()
+    {
+        $transaction = Transaction::factory()->create();
+
+        $this->get('/transactions')->assertSee($transaction->description);
+    }
+}
