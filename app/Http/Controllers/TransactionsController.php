722eb4e807d829fbe95398c6d16c825e64437dc9
th-drakopoulos <th.drakopoulos@tms.com.gr>
2022-02-27 14:32:52 +0200

    feat: lecture4

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
+
 class TransactionsController extends Controller
 {
     public function index()
     {
-
+        $transactions = Transaction::all();
+        return view('transactions.index', compact($transactions));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/', function () {
 });
 
 Route::get('/transactions', [TransactionsController::class, 'index']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
