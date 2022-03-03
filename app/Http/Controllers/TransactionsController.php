7facfbb56c28f67a9772d23aa3239dbab4fee590
th-drakopoulos <th.drakopoulos@tms.com.gr>
2022-03-03 22:09:07 +0200

    lecture10

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -7,6 +7,11 @@ use App\Models\Transaction;
 
 class TransactionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index(Category $category)
     {
         $transactions = Transaction::byCategory($category)->get();
